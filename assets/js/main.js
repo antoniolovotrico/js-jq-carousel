f226960b6cc1de20d1b06865af34f1ed3b1e912f
Antonio <antonio.lovotrico@icloud.com>
2020-11-27 18:46:10 +0100

    first part of the quest solved

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,43 @@
-$(function() {
-    var forward = $(".fa-angle-right");
-    var backward = $(".fa-angle-left");
-    var sliderElement = $(".images").find("img");
-    var i = 0;
-    var clickable = true;
+// $(function() {
+//     var forward = $(".fa-angle-right");
+//     var backward = $(".fa-angle-left");
+//     var sliderElement = $(".images").find("img");
+//     var i = 0;
+//     var clickable = true;
 
-    forward.click(function(){
-      if (clickable) {
-        clickable = false;
-        i++;
-
-
-
-
-
-
-
-
-
-      }
+//     forward.click(function(){
+//       if (clickable) {
+//         clickable = false;
+//         i++;    
+//       }
         
-    });
+//     });
     
-  });
+//   });
+
+$(function(){
+  var forward = $(".fa-angle-right");
+  var backward = $(".fa-angle-left");
+
+  forward.click(function(){
+
+    var imgAttiva = $("img.active");
+    var circleAttivo = $("i.active")
+    imgAttiva.removeClass("active");
+    imgAttiva.next().addClass("active");
+    circleAttivo.removeClass("active");
+    circleAttivo.next().addClass("active");
+
+    
+  })
+
+  backward.click(function(){
+    var imgAttiva = $("img.active");
+    var circleAttivo = $("i.active")
+    imgAttiva.removeClass("active");
+    imgAttiva.prev().addClass("active");
+    circleAttivo.removeClass("active");
+    circleAttivo.prev().addClass("active");
+  })
+
+})
