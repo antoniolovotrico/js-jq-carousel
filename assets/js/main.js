ef63906163c150d980d0f380552d02232344f9f0
Antonio <antonio.lovotrico@icloud.com>
2020-11-27 16:20:08 +0100

    wonking on general setting of jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,25 @@
 $(function() {
-    var forward = $("fa-angle-right");
-    var backward = $("fa-angle-left")
+    var forward = $(".fa-angle-right");
+    var backward = $(".fa-angle-left");
+    var sliderElement = $(".images").find("img");
+    var i = 0;
+    var clickable = true;
+
+    forward.click(function(){
+      if (clickable) {
+        clickable = false;
+        i++;
+
+
+
+
+
+
+
+
+
+      }
+        
+    });
     
   });
