3be4c523a03002b7c25e31de562a77cb06bec427
Antonio <antonio.lovotrico@icloud.com>
2020-11-28 13:14:43 +0100

    quest completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,51 @@
-// $(function() {
-//     var forward = $(".fa-angle-right");
-//     var backward = $(".fa-angle-left");
-//     var sliderElement = $(".images").find("img");
-//     var i = 0;
-//     var clickable = true;
-
-//     forward.click(function(){
-//       if (clickable) {
-//         clickable = false;
-//         i++;    
-//       }
-        
-//     });
-    
-//   });
-
 $(function(){
   var forward = $(".fa-angle-right");
-  var backward = $(".fa-angle-left");
-
+  var backward = $(".fa-angle-left"); 
+  
   forward.click(function(){
-
+     
+    var firstImg = $("img.first"); 
+    var firstCircle = $("i.first");
     var imgAttiva = $("img.active");
-    var circleAttivo = $("i.active")
-    imgAttiva.removeClass("active");
-    imgAttiva.next().addClass("active");
-    circleAttivo.removeClass("active");
-    circleAttivo.next().addClass("active");
-
+    var circleAttivo = $("i.active");
     
+    
+    if (imgAttiva.hasClass("last")) {
+      
+      imgAttiva.removeClass("active");
+      circleAttivo.removeClass("active");
+      firstImg.addClass("active");
+      firstCircle.addClass("active");
+
+    } else {
+      
+      imgAttiva.removeClass("active");
+      imgAttiva.next().addClass("active");
+      circleAttivo.removeClass("active");
+      circleAttivo.next().addClass("active");
+    }    
   })
 
   backward.click(function(){
+
     var imgAttiva = $("img.active");
-    var circleAttivo = $("i.active")
-    imgAttiva.removeClass("active");
-    imgAttiva.prev().addClass("active");
-    circleAttivo.removeClass("active");
-    circleAttivo.prev().addClass("active");
+    var circleAttivo = $("i.active");
+    var lastImg = $("img.last");
+    var lastCircle = $("i.last");
+
+    if (imgAttiva.hasClass("first")) {
+
+      imgAttiva.removeClass("active");
+      circleAttivo.removeClass("active");
+      lastImg.addClass("active");
+      lastCircle.addClass("active");
+    
+    } else {
+      imgAttiva.removeClass("active");
+      imgAttiva.prev().addClass("active");
+      circleAttivo.removeClass("active");
+      circleAttivo.prev().addClass("active");
+    }    
   })
 
 })
