ef72510d1a6834626194b4bc9be2d08159befc6e
Antonio <antonio.lovotrico@icloud.com>
2020-11-29 12:38:45 +0100

    added comment to JS section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
+//Handler Metod,tutto il codice JS racchiuso all'interno di questa funzione anonima verra caricato solo dopo il caricamento della DOM.
 $(function(){
+
+  //Dichiarazione ed inizializzazione delle variabili pulsanti. 
   var forward = $(".fa-angle-right");
   var backward = $(".fa-angle-left"); 
   
+  //Funzione forward per scorrere le slide da sinistra a destra
   forward.click(function(){
-     
+    
+    //Dichiarazione ed inizializzazione delle variabili Img e Circle delle slide. Dichiarare queste Var all'interno della funzione per un corretto funzionamento.
     var firstImg = $("img.first"); 
     var firstCircle = $("i.first");
     var imgAttiva = $("img.active");
     var circleAttivo = $("i.active");
     
-    
+    //Condizione valida nel caso in cui ci troviamo sull'ultima Img di uno Slider affinche ad un ulteriore click ricomincia a scorrere dalla prima Img.
     if (imgAttiva.hasClass("last")) {
       
       imgAttiva.removeClass("active");
@@ -17,6 +22,7 @@ $(function(){
       firstImg.addClass("active");
       firstCircle.addClass("active");
 
+    //Condizione generale per scorrere le immagini di uno slider da sinistra verso destra.      
     } else {
       
       imgAttiva.removeClass("active");
@@ -26,12 +32,15 @@ $(function(){
     }    
   })
 
+  //Funzione di scorrimento delle Img da sinistra a destra delle Img di uno slider alla pressione di un particolare tasto. Il resto delle condizioni è equivalente al click sul pulsante della funzione precedente.
   $(document).keydown(function(e){
+
       var firstImg = $("img.first"); 
       var firstCircle = $("i.first");
       var imgAttiva = $("img.active");
       var circleAttivo = $("i.active");
       
+      //il KeyCode 39 corrisponde alla arrow di destra della tastiera.
       if (e.keyCode == 39) { 
       
         if (imgAttiva.hasClass("last")) {
@@ -51,13 +60,16 @@ $(function(){
       }    
     })
 
+  //Funzione Backward per scorrere le slide da destra a sinistra.  
   backward.click(function(){
 
+    //Dichiarazione ed inizializzazione delle variabili Img e Circle delle slide. Dichiarare queste Var all'interno della funzione per un corretto funzionamento.
     var imgAttiva = $("img.active");
     var circleAttivo = $("i.active");
     var lastImg = $("img.last");
     var lastCircle = $("i.last");
 
+    //Condizione valida nel caso in cui ci troviamo sulla prima Img di uno Slider affinche ad un ulteriore click ricomincia a scorrere dall'ultima Img.
     if (imgAttiva.hasClass("first")) {
 
       imgAttiva.removeClass("active");
@@ -65,6 +77,7 @@ $(function(){
       lastImg.addClass("active");
       lastCircle.addClass("active");
     
+      //Condizione generale per scorrere le immagini di uno slider da destra verso sinistra.
     } else {
       imgAttiva.removeClass("active");
       imgAttiva.prev().addClass("active");
@@ -73,6 +86,7 @@ $(function(){
     }    
   })
 
+  //Funzione di scorrimento delle Img da destra a sinistra delle Img di uno slider alla pressione di un particolare tasto. Il resto delle condizioni è equivalente al click sul pulsante della funzione precedente.
   $(document).keydown(function(e){
     
     var imgAttiva = $("img.active");
@@ -80,6 +94,7 @@ $(function(){
     var lastImg = $("img.last");
     var lastCircle = $("i.last");
     
+    //il KeyCode 37 corrisponde alla arrow di sinistra della tastiera.
     if (e.keyCode == 37) { 
     
       if (imgAttiva.hasClass("first")) {
